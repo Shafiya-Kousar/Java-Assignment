3fc6b072b8c72bc154bb0526c3f0ff2791319c88
Shafiya-Kousar <shafiyakousar99@gmail.co>
2024-11-06 19:09:16 +0530

    committing

---
+++ Ass56_SubClass.java
@@ -0,0 +1,18 @@
+package assignment2;
+
+import assignment.Ass56_ParentClass;
+
+public class Ass56_SubClass extends Ass56_ParentClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ass56_ParentClass a=new Ass56_ParentClass();
+		a.show();
+		
+		Ass56_SubClass asc= new Ass56_SubClass();
+		asc.print();
+		asc.show();
+		
+	}
+
+}
---
+++ Ass57_AccSpec.java
@@ -0,0 +1,13 @@
+package assignment2;
+import assignment.Ass56_ParentClass;
+
+public class Ass57_AccSpec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Ass56_ParentClass a=new Ass56_ParentClass();
+		a.show();
+	}
+
+}
